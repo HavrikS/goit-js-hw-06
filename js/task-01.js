f368d23ce4b1f222724f33c4b01b7fb2eb55e097
Sergey Havrik <s.gavrik83@gmail.com>
2022-05-11 20:43:16 +0300

    виконав третє завдання

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,7 @@
-const categories = document.querySelector('#categories')
-console.log(`Number of categories: ${categories.children.length}`); 
+const categories = document.querySelector("#categories");
+console.log(`Number of categories: ${categories.children.length}`);
 
-
-[...categories.children].forEach(categorie => {
-console.log(`Category: ${categorie.firstElementChild.textContent}
-Elements: ${categorie.lastElementChild.children.length}`)
+[...categories.children].forEach((categorie) => {
+  console.log(`Category: ${categorie.firstElementChild.textContent}
+Elements: ${categorie.lastElementChild.children.length}`);
 });
